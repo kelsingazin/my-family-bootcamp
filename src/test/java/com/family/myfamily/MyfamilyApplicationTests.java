8be88bad442b5478e9ddc3d4a5178fe7b3e9987d
Shokhan Makhashov <shokhan.mv@gmail.com>
2023-03-01 15:41:15 +0600

    init commit

---
+++ src/test/java/com/family/myfamily/MyfamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.myfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
